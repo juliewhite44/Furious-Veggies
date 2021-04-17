dcf5abee29d0cc263929f181e30e612db9470914
lukasaristide <luk2002luk@gmail.com>
2021-04-18 00:43:08 +0200

    Setup for LibGDX

---
+++ core/test/com/io/project/Dummy.java
@@ -0,0 +1,20 @@
+package com.io.project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Dummy {
+
+	@Test
+	void test() {
+		assertTrue(true, "Should succeed");
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	void test2() {
+		assertTrue(false, "Should fail");
+	}
+
+}
